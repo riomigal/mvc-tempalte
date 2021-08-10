2ca74e9626297e7203184693ee776bbc29c960e4
Saverio Migale <saveriomigale@MacBook-Pro.local>
2021-08-10 23:02:48 +0200

    Routing and Views

--- cache/251ad8768faa23d3aaa99d271c70a636346189bb.php
+++ cache/251ad8768faa23d3aaa99d271c70a636346189bb.php
@@ -1,10 +1,21 @@
 <?php $__env->startSection('content'); ?>
 
-    <div class="row">
-        <div class="col-2 bg-dark">
-            <h1>test</h1>
+    <!-- Jumbotron -->
+    <div class="p-5 text-center bg-image rounded-3" style="
+        background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.jpg');
+        height: 400px;
+      ">
+        <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+            <div class="d-flex justify-content-center align-items-center h-100">
+                <div class="text-white">
+                    <h1 class="mb-3">Heading</h1>
+                    <h4 class="mb-3">Subheading</h4>
+                    <a class="btn btn-outline-light btn-lg" href="#!" role="button">Call to action</a>
+                </div>
+            </div>
         </div>
     </div>
+    <!-- Jumbotron -->
 <?php $__env->stopSection(); ?>
 
 <?php echo $__env->make('layout.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH /Users/saveriomigale/Desktop/PortfolioPage/resources/views/index.blade.php ENDPATH**/ ?>
